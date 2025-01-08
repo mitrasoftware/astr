c079d5baf91c0c49bc6ff6cc1d81752e8db7c09b
irshad Chanakya <35327176+mitrasoftware@users.noreply.github.com>
2025-01-08 19:49:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express  = require('express')
+const {generate}  = require('./calculate')
+ 
+const app = express()
+
+const PORT = 8000
+app.get('/users',   (req, res) =>   {
+
+   var  response = generate();
+    
+//    console.log(response.length)
+   res.json(  response  )
+
+    
+})
+
+app.listen(PORT,  () => console.log('Server started on port '+ PORT))
+
+ 
