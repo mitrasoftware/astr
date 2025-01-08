563e888e7c279efebd1b82a9260db14858b3d4ec
irshad Chanakya <35327176+mitrasoftware@users.noreply.github.com>
2025-01-08 20:28:50 +0530

    Update new  index.js

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 const express  = require('express')
 const {generate}  = require('./calculate')
+var bodyParser = require('body-parser');
  
 const app = express()
 
+
 const PORT = 8000
+
+app.use(bodyParser());
+
 app.get('/',   (req, res) =>   {
 
-   var  response = generate();
+    
     
 //    console.log(response.length)
-   res.json(  response  )
+   res.send("404")
 
     
 })
+
+app.post('/',   (req, res) =>   {
+
+     
+
+    const  name = req.body.name
+    const  dob = req.body.dob
+    const  timeIs = req.body.time_is
+    const  timeStamp = req.body.tiime_stamp
+    const  lat = req.body.latitude
+    const  long = req.body.longitude
+     
+
+
+    var  response = generate(
+           name,
+           dob,
+           timeIs,
+           timeStamp,
+           lat,
+           long)
+     
+ //    console.log(response.length)
+    res.json(  response  )
+ 
+     
+ })
 
 app.listen(PORT,  () => console.log('Server started on port '+ PORT))
 
